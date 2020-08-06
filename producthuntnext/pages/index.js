2cd8df8647db26500955409e63754ff5c420574e
Hiram Guerrero <hiramg90@gmail.com>
2020-08-06 10:56:04 -0500

    section 40 completed: displaying products index

--- producthuntnext/pages/index.js
+++ producthuntnext/pages/index.js
@@ -1,11 +1,51 @@
+import React, {useEffect, useState, useContext} from 'react';
 import Layout from "../components/layout/Layout";
+import {FirebaseContext} from '../firebase';
+import DetallesProducto from "../components/layout/DetallesProducto";
 
-const Home = () => (
+const Home = () => {
+
+  const [productos, guardarProductos] = useState([]);
+  const {firebase} = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos')
+        .orderBy('creado', 'desc')
+        .onSnapshot(manejarSnapshot);
+    }
+
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      }
+    });
+
+    guardarProductos(productos);
+  }
+
+  return (
     <div>
-        <Layout>
-            <h1>Inicio</h1>
-        </Layout>
+      <Layout>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {
+                productos.map(producto => (
+                  <DetallesProducto key={producto.id} producto={producto}/>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+      </Layout>
     </div>
-);
+  )
+}
 
 export default Home;
