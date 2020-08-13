0563a4afb9843147e2877fe8e90e338f1fc1d0b1
Hiram Guerrero <hiramg90@gmail.com>
2020-08-12 19:57:09 -0500

    section 50 completed: Nodesend - Users creation

--- nodesend/server/controllers/usuarioController.js
+++ nodesend/server/controllers/usuarioController.js
@@ -1,10 +1,33 @@
 const Usuario = require('./../models/Usuario');
+const bcrypt = require('bcrypt');
+const {validationResult} = require('express-validator');
 
 exports.nuevoUsuario = async (req, res) => {
-    //console.log(req.body);
 
-    const usuario = await new Usuario(req.body);
-    usuario.save();
+  //Mostrar mensajes de error
+  const errores = validationResult(req);
 
+  if (!errores.isEmpty()) {
+    return res.status(422).json({errores: errores.array()});
+  }
+
+  const {email, password} = req.body;
+
+  let usuario = await Usuario.findOne({email});
+
+  if (usuario) {
+    return res.status(400).json({msg: 'El usuario ya esta registrado'});
+  }
+
+  //Create new user
+  usuario = new Usuario(req.body);
+  const salt = await bcrypt.genSalt(10);
+  usuario.password = await bcrypt.hash(password, salt);
+
+  try {
+    await usuario.save();
     res.json({msg: 'Usuario creado correctamente'});
+  } catch (error) {
+    console.log(error);
+  }
 };
