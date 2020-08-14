dc5b7e3f4731f1e0b083cf1538625f664dc4fd1d
Hiram Guerrero <hiramg90@gmail.com>
2020-08-14 11:17:20 -0500

    section 55 completed: Nodesend - Auth forms

--- nodesend/client/components/Header.js
+++ nodesend/client/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div>
       <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-        <img src="/logo.svg" className="w-64 mb-8 md:mb-0"/>
+        <Link href="/">
+          <img src="/logo.svg" className="w-64 mb-8 md:mb-0"/>
+        </Link>
+
+        <div>
+          <Link href="/login"><a className="bg-red-500 px-5 py-3 rounded text-white font-bold uppercase mr-2">Iniciar
+            sesión</a></Link>
+          <Link href="/crearcuenta"><a
+            className="bg-black px-5 py-3 rounded text-white font-bold uppercase">Registrarse</a></Link>
+        </div>
       </header>
     </div>
   );
